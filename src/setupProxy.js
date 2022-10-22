776b3617d0eb95fe61c5de4f1b188bc9b750feb7
shetty-nithin <shettynithin744@gmail.com>
2022-10-22 14:14:17 +0530

    connected with localhost backend api to check

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,6 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = (app) => {
     app.use(
         '/v1',
-        createProxyMiddleware({ target: "https://roomzapi.herokuapp.com/roomz/api", changeOrigin: true})
+        // createProxyMiddleware({ target: "https://roomzapi.herokuapp.com/roomz/api", changeOrigin: true})
+        createProxyMiddleware({ target: "http://localhost:8080/roomz/api", changeOrigin: true})
     )
 }
